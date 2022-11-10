6ea5e624d10df35fdf508485b989fcf2ed9e3e92
Miliexe <emilie.lblc@gmail.com>
2022-11-10 07:51:47 +0100

    Init Footer.jsx

---
+++ src/components/LogoWhite.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LogoWhite = () => {
+    return (
+        <div>
+            <img
+                className="footer__logo"
+                src="./logo_white.png"
+                alt="logo Kasa"
+            />
+        </div>
+    )
+}
+
+export default LogoWhite
