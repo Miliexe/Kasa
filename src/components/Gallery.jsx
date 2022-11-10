d3d90da00f4f8804a8ed4b331623bd9b1cf22992
Miliexe <emilie.lblc@gmail.com>
2022-11-10 06:01:41 +0100

    Link to Database

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import Data from '../data/logements.json'
+import { Link } from 'react-router-dom'
 
 const Gallery = () => {
     return (
         <div className="gallery">
-            <div className="gallery__card"></div>
-            <div className="gallery__card"></div>
-            <div className="gallery__card"></div>
-            <div className="gallery__card"></div>
-            <div className="gallery__card"></div>
-            <div className="gallery__card"></div>
+            {Data.map((item) => {
+                return (
+                    <Link
+                        to={`/product/${item.id}`}
+                        className="gallery__card"
+                        key={item.id}
+                    >
+                        <img src={item.cover} alt={item.title} />
+                    </Link>
+                )
+            })}
         </div>
     )
 }
