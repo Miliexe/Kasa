62048be1b7b1a1654f52f07ea20fb533518939aa
Miliexe <emilie.lblc@gmail.com>
2022-11-10 03:26:10 +0100

    Init HomeBanner.jsx

---
+++ src/components/HomeBanner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HomeBanner = () => {
+    return (
+        <div className="home_banner">
+            <div className="home_banner__image">
+                <h1 className="home_banner__title">
+                    Chez vous,
+                    <br />
+                    partout et ailleurs
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default HomeBanner
