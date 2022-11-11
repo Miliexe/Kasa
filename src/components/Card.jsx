7e6db1aacdd62bc14e5c93d752860bfbf6b53356
Miliexe <emilie.lblc@gmail.com>
2022-11-11 23:32:42 +0100

    Fix Sass

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,10 @@ import { Link } from 'react-router-dom'
 
 const Card = ({ id, cover, title }) => {
     return (
-        <div>
-            <Link to={`/product/${id}`} className="gallery__card" key={id}>
-                <img src={cover} alt={title} />
-                <h3 className="card__title">{title}</h3>
-            </Link>
-        </div>
+        <Link to={`/product/${id}`} className="gallery__card" key={id}>
+            <img src={cover} alt={title} />
+            <h3 className="card__title">{title}</h3>
+        </Link>
     )
 }
 
