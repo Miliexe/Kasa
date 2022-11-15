8187f3cea7fbe363cf205dc9d7faafc846d645f1
Miliexe <emilie.lblc@gmail.com>
2022-11-15 02:32:33 +0100

    Update Slideshow

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,74 +1,67 @@
-import React from 'react'
-import { Slide } from 'react-slideshow-image'
-import 'react-slideshow-image/dist/styles.css'
-import img1 from '../asset/carrousel.png'
+import React, { useState } from 'react'
 
-const buttonStyle = {
-    background: 'none',
-    border: '0px',
-    padding: '0px 25px',
-}
+const Slideshow = (props) => {
+    let [displayedImg, setDisplayedImg] = useState(0)
 
-const proprietes = {
-    duration: 10000,
-    transitionDuration: 1000,
-    infinite: true,
-    indicators: true,
-    arrow: true,
-    prevArrow: (
-        <button style={{ ...buttonStyle }}>
-            <svg
-                width="48"
-                height="80"
-                viewBox="0 0 48 80"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <path
-                    d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z"
-                    fill="white"
-                />
-            </svg>
-        </button>
-    ),
-    nextArrow: (
-        <button style={{ ...buttonStyle }}>
-            <svg
-                width="48"
-                height="80"
-                viewBox="0 0 48 80"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <path
-                    d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z"
-                    fill="white"
-                />
-            </svg>
-        </button>
-    ),
-}
+    const next = () => {
+        setDisplayedImg((displayedImg = displayedImg + 1))
 
-const Slideshow = () => {
+        if (displayedImg + 1 > props.img.length) {
+            setDisplayedImg(0)
+        }
+    }
+
+    const previous = () => {
+        setDisplayedImg((displayedImg = displayedImg - 1))
+
+        if (displayedImg < 0) {
+            setDisplayedImg(props.img.length - 1)
+            return
+        }
+    }
+
     return (
         <div className="slideshow">
-            <Slide {...proprietes}>
-                <div className="slide">
-                    <div>
-                        <img src={img1} alt="img1" />
-                    </div>
-                </div>
-                <div className="slide">
-                    <div>
-                        <img src={img1} alt="img1" />
-                    </div>
-                </div>
-                <div className="slide">
-                    <div>
-                        <img src={img1} alt="img1" />
-                    </div>
-                </div>
-            </Slide>
+            <button className="button button--left" onClick={previous}>
+                <svg
+                    width="48"
+                    height="80"
+                    viewBox="0 0 48 80"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z"
+                        fill="white"
+                    />
+                </svg>
+            </button>
+            <button className="button button--right" onClick={next}>
+                <svg
+                    width="48"
+                    height="80"
+                    viewBox="0 0 48 80"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z"
+                        fill="white"
+                    />
+                </svg>
+            </button>
+
+            <div className="slide">
+                <img
+                    src={props.img && props.img[displayedImg]}
+                    alt={props.title}
+                />
+            </div>
+
+            <span className="indicators">
+                {displayedImg ? displayedImg + 1 : 1}/
+                {props.img && props.img.length}
+            </span>
         </div>
     )
 }
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,21 @@
 import Slideshow from '../components/Slideshow'
+import Data from '../data/logements.json'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 function Product() {
+    const [product, setProduct] = useState(Data)
+
+    const { id } = useParams()
+
+    useEffect(() => {
+        const display = Data.filter((item) => item.id === id)[0]
+        setProduct(display)
+    }, [id])
+
     return (
-        <div>
-            <Slideshow />
+        <div className="product">
+            <Slideshow img={product.pictures} />
         </div>
     )
 }
