6900435ac846975d83c725c3de0af7a21998f3ec
Miliexe <emilie.lblc@gmail.com>
2022-11-25 00:09:30 +0100

    Init _card.scss

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 const Card = ({ id, cover, title }) => {
     return (
-        <Link to={`/product/${id}`} className="gallery__card">
-            <img src={cover} alt={title} />
+        <Link to={`/product/${id}`} className="card">
+            <img className="card__img" src={cover} alt={title} />
             <h3 className="card__title">{title}</h3>
         </Link>
     )
